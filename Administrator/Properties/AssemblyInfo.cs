a433122f3d596d92b958c19e40b77de5566e5866
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2023-12-08 15:05:50 +0300

    beginning

--- Administrator/Properties/AssemblyInfo.cs
+++ Administrator/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("WaterWorldSalon")]
+[assembly: AssemblyTitle("Administrator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WaterWorldSalon")]
+[assembly: AssemblyProduct("Administrator")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Courier/Properties/Resources.Designer.cs
+++ Courier/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WaterWorldSalon.Properties
+namespace Courier.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace WaterWorldSalon.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WaterWorldSalon.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Courier.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
